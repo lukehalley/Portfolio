8110878f7a724879207c9db78b6cdff5bc02073f
Luke Halley <luke123halley@gmail.com>
2016-12-15 11:26:34 +1000

    Add validation logic for component naming

--- lukehalleyPortfolio/semantic/tasks/admin/components/create.js
+++ lukehalleyPortfolio/semantic/tasks/admin/components/create.js
@@ -19,6 +19,7 @@
  * @returns {Promise} Resolution of component creation
  */
 // Initialize scaffolding for new semantic UI components
+// Validate component name format and check for naming conflicts
 // Creates a new semantic component with proper structure
  * Creates a new Semantic UI component with default structure
  * @param {string} componentName - Name of component to create
