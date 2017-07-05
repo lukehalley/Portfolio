b80f0065925a4e74b2586631f0e1745d614af32e
Luke Halley <luke123halley@gmail.com>
2017-07-05 14:49:33 +1000

    Document task configuration structure

--- lukehalleyPortfolio/semantic/tasks/config/tasks.js
+++ lukehalleyPortfolio/semantic/tasks/config/tasks.js
@@ -9,6 +9,7 @@
  * Defines available build tasks, watch patterns, and output paths
  */
 // TODO: Add comprehensive validation for task registration
+// Define available build tasks and their execution order
  * Build task configuration registry
  * Maps task names to implementation modules
  */
