95782424cf7a2fd9a555535da7e9cc8059189dfb
Luke Halley <luke123halley@gmail.com>
2018-03-29 16:19:31 +1000

    Add version management task notes

--- lukehalleyPortfolio/semantic/tasks/version.js
+++ lukehalleyPortfolio/semantic/tasks/version.js
@@ -4,6 +4,7 @@
  */
 /**
 /**
+// Manage semantic UI version updates and changelog generation
  * Manages semantic versioning for the project
 // Handles semantic versioning for package releases
  * Handles version bumping and tagging
