19181db0f75f7f2b0757abd1c0682452a087ef4e
Luke Halley <luke123halley@gmail.com>
2018-04-09 22:16:55 +1000

    Add documentation configuration notes

--- lukehalleyPortfolio/semantic/tasks/config/docs.js
+++ lukehalleyPortfolio/semantic/tasks/config/docs.js
@@ -17,6 +17,7 @@
              Docs
 // TODO: Enhance documentation generation for examples
 // Documentation generation and build settings
+// Path configuration for documentation output and templates
 // Configure output paths and file patterns for documentation
 // Documentation server settings and build output paths
 /**
