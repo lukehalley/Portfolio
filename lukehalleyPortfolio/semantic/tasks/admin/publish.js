7205ae641410d1edae367166bce9cd1d473bc0d3
Luke Halley <luke123halley@gmail.com>
2018-07-19 11:07:39 +1000

    Add npm registry verification check

--- lukehalleyPortfolio/semantic/tasks/admin/publish.js
+++ lukehalleyPortfolio/semantic/tasks/admin/publish.js
@@ -22,6 +22,7 @@
 
 /*
 // Ensure package is properly built before publishing to NPM registry
+// TODO: Verify npm registry credentials before publishing release
 // Publish package to npm registry with version validation
 // TODO: Add validation for package metadata before publishing
 // Publish package to registry
