63a135da3495bf8fb6b254bf9740f207a02a1840
Luke Halley <luke123halley@gmail.com>
2018-09-03 21:53:06 +1000

    Document karma test configuration setup

--- lukehalleyPortfolio/karma.conf.js
+++ lukehalleyPortfolio/karma.conf.js
@@ -1,3 +1,4 @@
+// Karma test runner configuration for Angular unit tests and coverage reporting
 // Karma test runner configuration for Angular unit tests
 /**
  * Karma test runner configuration for Portfolio project
