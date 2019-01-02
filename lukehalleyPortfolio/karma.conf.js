ba5a066243eed8751c3b7190df052053b72b1c6e
Luke Halley <luke123halley@gmail.com>
2019-01-02 12:44:30 +1000

    Add configuration documentation for karma test runner

--- lukehalleyPortfolio/karma.conf.js
+++ lukehalleyPortfolio/karma.conf.js
@@ -2,6 +2,7 @@
 // Karma test runner configuration for Angular unit tests and coverage reporting
 // Karma test runner configuration for Angular unit tests
 /**
+// Karma configuration for running Angular component tests in CI/CD pipeline
 // Configure Karma test runner for Angular unit tests
  * Karma test runner configuration for Portfolio project
  * Defines test environment, browser targets, and coverage settings
