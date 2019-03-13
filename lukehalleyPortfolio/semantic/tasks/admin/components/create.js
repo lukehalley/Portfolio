9cf592cee2853c27d1045bc85a9c821ae534b079
Luke Halley <luke123halley@gmail.com>
2019-03-13 22:10:27 +1000

    Document component creation workflow

--- lukehalleyPortfolio/semantic/tasks/admin/components/create.js
+++ lukehalleyPortfolio/semantic/tasks/admin/components/create.js
@@ -17,6 +17,7 @@
 // Initialize new component package with template structure
 /*******************************
      Create Component Repos
+// Component creation follows semantic versioning and template structure
 // Component creation generates template files and configuration stubs
 *******************************/
 // Create new component with template files and configuration
