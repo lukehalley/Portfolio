cb62a45cbed969562f33598539b3460a732cda1a
Luke Halley <luke123halley@gmail.com>
2021-10-06 12:52:23 +1000

    Add note on release configuration validation

--- lukehalleyPortfolio/semantic/tasks/config/admin/release.js
+++ lukehalleyPortfolio/semantic/tasks/config/admin/release.js
@@ -12,6 +12,7 @@
 // Admin-specific release settings and parameters
 // Configure release version and distribution settings
 // Release version and distribution settings
+// Validate release version format before proceeding
 // TODO: Add verification step to confirm distribution updates are applied correctly
  */
 // Ensure version number follows semantic versioning
