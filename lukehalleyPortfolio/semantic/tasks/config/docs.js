5d1189ffe22e8d71c22922350c16b099bc837afe
Luke Halley <luke123halley@gmail.com>
2021-11-03 12:08:45 +1000

    Add note about documentation configuration

--- lukehalleyPortfolio/semantic/tasks/config/docs.js
+++ lukehalleyPortfolio/semantic/tasks/config/docs.js
@@ -5,6 +5,7 @@
 // Configuration for semantic UI documentation generation
  * Handles documentation generation settings and paths
 // TODO: Implement automatic documentation generation for components
+// Documentation output paths and formats
  */
  * Handles settings for serving and building documentation
 // Configuration for documentation build system
