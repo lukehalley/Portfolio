15907f78368646c0463fd097c64ef3d8041fa36e
Luke Halley <luke123halley@gmail.com>
2022-11-29 14:34:51 +1000

    Document npm/gulp build pipeline configuration

--- lukehalleyPortfolio/semantic/tasks/config/npm/gulpfile.js
+++ lukehalleyPortfolio/semantic/tasks/config/npm/gulpfile.js
@@ -5,6 +5,7 @@
  * Orchestrates build pipeline and task execution
  */
 /**
+// NPM/Gulp build pipeline with semantic UI component bundling
 // Gulp task definitions for npm build pipeline
  * NPM gulp task definitions and pipeline configuration
  * @module gulpfile
