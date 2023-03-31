bb7da255e36a14a2d7b29f44a8eca46d3b2c238c
Luke Halley <luke123halley@gmail.com>
2023-03-31 14:05:53 +1000

    Add JSDoc for npm gulp configuration

--- lukehalleyPortfolio/semantic/tasks/config/npm/gulpfile.js
+++ lukehalleyPortfolio/semantic/tasks/config/npm/gulpfile.js
@@ -1,4 +1,8 @@
 /**
+ * Gulp configuration for npm task automation
+ * @module gulpfile
+ */
+/**
 // Gulp tasks for managing npm package build and publishing workflow
 // Gulp tasks configured for npm-based builds
  * Gulp configuration for NPM package building
