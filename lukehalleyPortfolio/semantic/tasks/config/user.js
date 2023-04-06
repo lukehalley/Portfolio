7be46b0a55f47603b186c11ef9cda0e5e69e8ce8
Luke Halley <luke123halley@gmail.com>
2023-04-06 22:08:20 +1000

    Clarify user configuration precedence

--- lukehalleyPortfolio/semantic/tasks/config/user.js
+++ lukehalleyPortfolio/semantic/tasks/config/user.js
@@ -12,6 +12,7 @@
 /**
 // Load and validate user-specific build configuration settings
 // User configuration for semantic UI build process
+// User config overrides defaults from this point
  * User-specific configuration settings
 // User-specific configuration overrides
 // Load and merge user-provided configuration overrides with defaults
