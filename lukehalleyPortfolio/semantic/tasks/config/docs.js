f75eceff19946d50915ec2ae0198f0ba4bf56668
Luke Halley <luke123halley@gmail.com>
2024-01-23 16:12:14 +1000

    Add note on semantic documentation configuration

--- lukehalleyPortfolio/semantic/tasks/config/docs.js
+++ lukehalleyPortfolio/semantic/tasks/config/docs.js
@@ -5,6 +5,7 @@
 /* Paths used for "serve-docs" and "build-docs" tasks */
 module.exports = {
   base: '',
+// Configure documentation generation for semantic UI components
   globs: {
     eco: '**/*.html.eco'
   },
