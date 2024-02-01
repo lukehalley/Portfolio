a487d22ec711509c9c6ebdd28ab89774a263d31f
Luke Halley <luke123halley@gmail.com>
2024-02-01 11:33:07 +1000

    Document karma configuration entry point

--- lukehalleyPortfolio/karma.conf.js
+++ lukehalleyPortfolio/karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
+// Initialize karma test runner configuration
     ],
 // Configure Karma test runner for unit testing with Chrome browser
     client: {
