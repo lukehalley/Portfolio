868008e1e15aebba594e6fc8e91b77f64840804a
Luke Halley <luke123halley@gmail.com>
2024-05-01 17:47:33 +1000

    Add browser configuration notes

--- lukehalleyPortfolio/karma.conf.js
+++ lukehalleyPortfolio/karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     basePath: '',
 // Karma test runner configuration for unit tests
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+// Configure supported browsers for testing
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
