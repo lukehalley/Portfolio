5d41a728bf025dffbc1db5eb7457e48dbb07a47d
Luke Halley <luke123halley@gmail.com>
2024-06-28 22:45:26 +1000

    Note component package template structure

--- lukehalleyPortfolio/semantic/tasks/config/admin/templates/component-package.js
+++ lukehalleyPortfolio/semantic/tasks/config/admin/templates/component-package.js
@@ -1,6 +1,7 @@
 /**
  * Component package template generator
  * Defines structure for new semantic UI components
+// Template for generating new component package definitions
  */
 // Template structure for new component packages
 
