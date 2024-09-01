8476b5c4ccfb474a6f12b49404b3717ee07c7991
Luke Halley <luke123halley@gmail.com>
2024-09-02 09:24:55 +1000

    Add JSDoc header to karma configuration

--- lukehalleyPortfolio/karma.conf.js
+++ lukehalleyPortfolio/karma.conf.js
@@ -1,3 +1,7 @@
+/**
+ * Karma test runner configuration
+ * Configures testing framework and browser settings
+ */
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
