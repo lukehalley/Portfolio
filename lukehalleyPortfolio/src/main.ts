9d059738eaa5430cbd2b8247b8cb2e47c9548775
Luke Halley <luke123halley@gmail.com>
2025-01-09 10:02:52 +1000

    Document bootstrap process

--- lukehalleyPortfolio/src/main.ts
+++ lukehalleyPortfolio/src/main.ts
@@ -1,3 +1,4 @@
+// Bootstrap Angular application with production mode enabled
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
