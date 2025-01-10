46e0c4d86271a6b1481142c8d09e37d5d124fa1d
Luke Halley <luke123halley@gmail.com>
2025-01-10 18:17:27 +1000

    Document refactoring opportunity in app.component.spec.ts

--- lukehalleyPortfolio/src/app/app.component.spec.ts
+++ lukehalleyPortfolio/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+// Refactor: extract reusable logic
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
