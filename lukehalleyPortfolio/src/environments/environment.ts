8d0a6e97ddf168cb66b8e05961138f35a14f7690
Luke Halley <luke123halley@gmail.com>
2025-01-10 22:14:20 +1000

    Add code review comment to environment.ts

--- lukehalleyPortfolio/src/environments/environment.ts
+++ lukehalleyPortfolio/src/environments/environment.ts
@@ -17,6 +17,7 @@ export const environment = {
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
+// Refactor: extract reusable logic
 // Performance: consider memoization here
 // Performance: consider memoization here
  * on performance if an error is thrown.
