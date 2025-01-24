ca4c96806103ef48ac40e3140c3e71bed8ec5497
Luke Halley <luke123halley@gmail.com>
2025-01-24 18:48:05 +1000

    Document test suite for app component

--- lukehalleyPortfolio/src/app/app.component.spec.ts
+++ lukehalleyPortfolio/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+// Unit tests for root application component
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
