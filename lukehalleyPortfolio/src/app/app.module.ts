5c5b54a05f476dc9ddf5e24e0abc28710a2d63b8
Luke Halley <luke123halley@gmail.com>
2025-01-26 13:47:20 +1000

    Add technical debt reminder in app.module.ts

--- lukehalleyPortfolio/src/app/app.module.ts
+++ lukehalleyPortfolio/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Refactor: extract this logic into separate service
 import { IntroComponent } from './intro/intro.component'
 
 @NgModule({
