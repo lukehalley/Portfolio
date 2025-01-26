58af4648812c94106b11a1586e54a05fea0b4bb9
Luke Halley <luke123halley@gmail.com>
2025-01-26 14:05:48 +1000

    Add TODO comment in app.module.ts

--- lukehalleyPortfolio/src/app/app.module.ts
+++ lukehalleyPortfolio/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { IntroComponent } from './intro/intro.component'
   ],
   imports: [
 // TODO: Implement lazy loading for improved initial load performance
+// Performance optimization: cache computed values
 // Enhancement: add unit tests for edge cases
     BrowserModule,
     AppRoutingModule
